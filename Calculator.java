789f089596cb7f6642b6b8bc61891c474c074a52
Chandana Sri <84367898+ChandanaSriKandi@users.noreply.github.com>
2022-06-05 12:44:11 +0530

    Feature branch (#2)
    * adding the Calculator class
    
    * commiting the changes
    
    * chandu
    
    * commiting the product method in the calculator

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+/**
+* This class contains the functions related to basic arithmetic operators
+* @since 1.0
+* @version 1.0
+*/
+
+public class Calculator{
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public int product(int a,int b){
+        return a*b;
+    }
+    public int product(int a,int b){
+        return a*b;
+    }
+    }
